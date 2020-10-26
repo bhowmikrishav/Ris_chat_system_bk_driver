ae086d2c2d60647d078b2743d5356afa6a79e754
Rishav Bhowmik <tgs.rishav.bhowmik@gmail.com>
2020-10-26 20:54:20 +0530

    concept debugged

--- operations/message.js
+++ operations/message.js
@@ -3,22 +3,43 @@ const mysql_config = require('../../configs/mysql_config.json')
 const net_fm_request = require('./net_fm_request')
 
 class MessageObject{
-    constructor(){
-        this.lines = []
+    constructor(message_buffer = null){
+        const this_class = this
+        try {
+            this_class.time = Date.now()
+            this_class.lines = []
+            if(message_buffer){
+                const {header, body} = net_fm_request.antonym(message_buffer)
+                this_class.time = header.send_at_ms
+
+                var start_point = 0;
+
+                for(const l in header.buffer_map){
+                    const end_point = start_point + header.buffer_map[l];
+                    this_class.lines.push(
+                        message_buffer.subarray( start_point, end_point)
+                    )
+                    start_point += end_point
+                }
+            }
+        } catch (e) {
+            throw e
+        }
     }
     add_line(header, data = ''){
         this.lines.push(
-            net_fm_request.capsule(
-                header,
-                data
-            )
+            net_fm_request.capsule( header, data)
         )
     }
     get_line(index){
         return net_fm_request.antonym(this.lines[index])
     }
     get_message_buffer(){
-        return Buffer.concat(this.lines)
+        const buffer_map = this.lines.map((obj)=>{return Number(obj.length)})
+        return net_fm_request.capsule( 
+            {send_at_ms:this.time, buffer_map:buffer_map},
+            Buffer.concat(this.lines)
+        )
     }
 }
 
@@ -71,7 +92,12 @@ module.exports = {Messanger, SendMessage}
 
 //unit tests
 const lines = new MessageObject()
-lines.add_line({type:'text'})
+lines.add_line({type:'text'}, "Hellow")
+lines.add_line({type:'text'}, "Hello Havi")
 
 console.log(lines.get_message_buffer())
-console.log(lines.get_line(0).body.toString())
+
+const best_ = new MessageObject(lines.get_message_buffer())
+
+console.log(best_.lines[0].length, best_.lines[1].length);
+//console.log(best_.get_line(0));
